5b4334cd6c553251bc14d8a28e6fa615825984c1
Maryclare Griffin <maryclaregriffin@gmail.com>
2017-08-28 10:53:56 -0400

    Fixed starting value to be inside limiting uniform distribution bounds

--- R/powreg.R
+++ R/powreg.R
@@ -18,6 +18,10 @@ powreg <- function(y, X, sigma.sq, tau.sq, q, samples =  50000) {
   A <- crossprod(X)
   del <- (1 - min(eigen(A)$values))
   start <- as.numeric(crossprod(solve(A + del*diag(ncol(X))), crossprod(X, y)))
+  # If q is bigger than 2, get starting values within unif. dist. bounds
+  if (q > 2) {
+    start[abs(start) > sqrt(3)*tau.sq] <- sign(start[abs(start) > sqrt(3)*tau.sq])*sqrt(3)*tau.sq
+  }
   
   # I think this is a replicable way of setting a seed, assuming a seed has been
   # set in R
